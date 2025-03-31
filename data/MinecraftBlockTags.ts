03ffa008fa3aa42d84dc76ea48ee33e29e291cc7
Qianshanyao <qian_shan_yao@163.com>
2025-03-31 12:50:31 +0800

    feat(block): 导出1.21.7版本方块tag

--- data/MinecraftBlockTags.ts
+++ data/MinecraftBlockTags.ts
@@ -1,19 +1,43 @@
 import {MinecraftIdRefs} from "../lib/MinecraftIdRefs";
 
 export class MinecraftBlockTags extends MinecraftIdRefs {
-  public static Logs = this.get('logs');
+  static IsPickaxeItemDestructible = this.get('minecraft:is_pickaxe_item_destructible');
+  static IsShovelItemDestructible = this.get('minecraft:is_shovel_item_destructible');
+  static IsAxeItemDestructible = this.get('minecraft:is_axe_item_destructible');
+  static IronTierDestructible = this.get('minecraft:iron_tier_destructible');
+  static StoneTierDestructible = this.get('minecraft:stone_tier_destructible');
+  static IsHoeItemDestructible = this.get('minecraft:is_hoe_item_destructible');
+  static Crop = this.get('minecraft:crop');
+  static DiamondTierDestructible = this.get('minecraft:diamond_tier_destructible');
+  static IsShearsItemDestructible = this.get('minecraft:is_shears_item_destructible');
+  static IsSwordItemDestructible = this.get('minecraft:is_sword_item_destructible');
 
-  static IsPickaxeItemDestructible = this.get('is_pickaxe_item_destructible');
-  static IsHoeItemDestructible = this.get('is_hoe_item_destructible');
-  static IsShovelItemDestructible = this.get('is_shovel_item_destructible');
-  static IsAxeItemDestructible = this.get('is_axe_item_destructible');
-  static IsSwordItemDestructible = this.get('is_sword_item_destructible');
-  static IsShearsItemDestructible = this.get('is_shears_item_destructible');
-
-  static StoneTierDestructible = this.get('stone_tier_destructible');
-  static IronTierDestructible = this.get('iron_tier_destructible');
-  static DiamondTierDestructible = this.get('diamond_tier_destructible');
-
-
+  static FertilizeArea_Experimental = 'fertilize_area';
+  static Gravel_Experimental = 'gravel';
+  static Rail_Experimental = 'rail';
+  static Wood_Experimental = 'wood';
+  static TextSign_Experimental = 'text_sign';
+  static IronPickDiggable_Experimental = 'iron_pick_diggable';
+  static DiamondPickDiggable_Experimental = 'diamond_pick_diggable';
+  static StonePickDiggable_Experimental = 'stone_pick_diggable';
+  static OneWayCollidable_Experimental = 'one_way_collidable';
+  static Stone_Experimental = 'stone';
+  static Trapdoors_Experimental = 'trapdoors';
+  static Snow_Experimental = 'snow';
   static Dirt_Experimental = 'dirt';
+  static Log_Experimental = 'log';
+  static Jungle_Experimental = 'jungle';
+  static Plant_Experimental = 'plant';
+  static Oak_Experimental = 'oak';
+  static Metal_Experimental = 'metal';
+  static NotFeatureReplaceable_Experimental = 'not_feature_replaceable';
+  static Spruce_Experimental = 'spruce';
+  static Grass_Experimental = 'grass';
+  static MobSpawner_Experimental = 'mob_spawner';
+  static Birch_Experimental = 'birch';
+  static Sand_Experimental = 'sand';
+  static Pumpkin_Experimental = 'pumpkin';
+  static Water_Experimental = 'water';
+  static DarkOak_Experimental = 'dark_oak';
+  static Acacia_Experimental = 'acacia';
 }
